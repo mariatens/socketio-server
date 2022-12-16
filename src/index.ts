860fe32a6b4fbd730ee97c516559b5e05d92cf8c
María Ten <49565433+mariatens@users.noreply.github.com>
2022-12-16 09:27:21 +0000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,70 @@
+import * as cors from "cors";
+import * as express from "express";
+import * as http from "http";
+import { Server, Socket } from "socket.io";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const server = http.createServer(app);
+
+// make a socket.io server object.
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    },
+});
+
+//our "database"
+const storyWords: string[] = [];
+
+io.on("connection", (s: Socket) => {
+    console.log("Got a connection.  Registering handlers...");
+
+    //useful for debugging
+    s.onAny((tag: string, ...otherArgs) =>
+        console.log("Received: ", tag, otherArgs, "(generic handler)")
+    );
+
+    //register to handle specific messages...
+    s.on("addWord", (newWord: string) => {
+        storyWords.push(newWord);
+        //tell everyone, including socket s
+        io.emit("storyUpdate", storyWords);
+    });
+
+    s.on("clearStory", (newWord: string) => {
+        storyWords.length = 0; //reset array to [], even though const.
+        //Tell everyone, including socket s
+        io.emit("storyUpdate", storyWords);
+    });
+
+    s.on("sayToOthers", (msg: string) => {
+        //Sends to all connected, EXCEPT not to this socket, s
+        s.broadcast.emit("chat", { from: s.id, msg: msg });
+    });
+});
+
+//Send something every 20 seconds to all connected
+setInterval(() => {
+    io.emit("time", new Date());
+}, 20000);
+
+//express routes - optional
+app.get("/", (req, res) => {
+    res.send("try POSTing {word: 'blah'} to /storyWords");
+});
+
+app.post("/storyWords", (req, res) => {
+    const word = req.body.word;
+    storyWords.push(word);
+    res.status(201).json({ outcome: "success", storyWords: storyWords });
+    io.emit("storyUpdate", storyWords);
+});
+
+const port = process.env.PORT ?? 4000;
+//important: call listen on the *server*, not the `app` directly.
+server.listen(port, () => {
+    console.log("socketio and express server listening on *:" + port);
+});
